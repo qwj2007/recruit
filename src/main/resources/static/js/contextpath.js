fff8a9343840611fb997a6d091bbc34fa369553f
qwj2007 <qwj2007@163.com>
2019-12-28 21:29:27 +0800

    修改js

--- src/main/resources/static/js/contextpath.js
+++ src/main/resources/static/js/contextpath.js
@@ -1,6 +1,5 @@
 function getContextPath() {
     var pathName = document.location.pathname;
-    alert(pathName)
     var index = pathName.substr(1).indexOf("/");
     var result = pathName.substr(0,index+1);
     return result;
